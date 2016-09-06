974336da30c17b157e0da0eb3bbc030af94354ee
Megha Bhatt <megha.bhatt@shopify.com>
2016-09-05 20:16:36 -0400

    Final Changes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
-var speed = Math.random();
 var xTileSize = 101;
 var yTileSize = 83;
-var xBound = 505;
-var yBound = 415;
 
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+var Enemy = function() {
   this.sprite = 'images/enemy-bug.png';
   this.x = 0;
   this.y = (Math.floor(Math.random() * 3) + 1) * yTileSize - (yTileSize / 2)
@@ -14,8 +11,6 @@ var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    // this.height = 80;
-    // this.width = 100;
   var obj = Object.create(Enemy.prototype);
 };
 
@@ -37,6 +32,7 @@ Enemy.prototype.render = function() {
 }
 
 Enemy.prototype.getspeed = function(){
+  var speed = Math.random();
   var numone = speed + 300;
   var numtwo = speed + 100;
   return Math.floor(Math.random() * (numone - numtwo + 1)) + numtwo;
@@ -54,11 +50,23 @@ var Player = function(x, y) {
 };
 
 Player.prototype.update = function() {
-  if (player.y === enemyone.y && player.x === enemyone.x
-    || player.y === enemytwo.y && player.x === enemytwo.x
-    || player.y === enemythree.y && player.x === enemythree.x){
-    player.reset();
-  };
+  // 2D Collision Detection - https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+  for (i = 0; i < allEnemies.length; i++) {
+    if (player.x < allEnemies[i].x + xTileSize &&
+      player.x + xTileSize > allEnemies[i].x &&
+      player.y < allEnemies[i].y + yTileSize &&
+      yTileSize + player.y > allEnemies[i].y) {
+       player.reset();
+    }
+  }
+
+  // Determine win condition
+  // If player reaches water, reset it.
+  if (player.y <= yTileSize/3) {
+      window.alert ('You win!');
+      player.reset();
+    };
+
 };
 
 Player.prototype.render = function() {
@@ -92,20 +100,18 @@ Player.prototype.handleInput = function(e){
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var enemyone = new Enemy(0, 50, speed * 100);
-var enemytwo = new Enemy(0, 100, speed * 200);
-var enemythree = new Enemy(0, 200, speed * 50);
+var enemyone = new Enemy();
+var enemytwo = new Enemy();
+var enemythree = new Enemy();
 var allEnemies = [enemyone, enemytwo, enemythree];
 allEnemies.push;
 
 var player = new Player();
 
 Player.prototype.reset = function() {
-  this.x = 200;
-  this.y = 413;
+  this.x = xTileSize * 2;
+  this.y = yTileSize * 4.5;
 };
-
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
